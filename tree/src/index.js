d8414e3a0d96fdd29d007e57a6a2e57338eea865
Rijwan Hossain <rijyanhossain@gmail.com>
2019-07-13 20:41:14 -0700

    Project setup done

--- tree/src/index.js
+++ tree/src/index.js
@@ -4,9 +4,15 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import '../node_modules/material-design-icons/iconfont/material-icons.css'
+import '../node_modules/materialize-css/dist/css/materialize.css'
+import M from '../node_modules/materialize-css/dist/js/materialize.js'
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+export default M; 
